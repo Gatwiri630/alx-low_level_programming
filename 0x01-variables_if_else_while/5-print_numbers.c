7439f329fac7b0672f154944c69cc3975e1236b4
Gatwiri630 <gatwiriesther630@gmail.com>
2022-12-11 13:36:37 -0800

    make fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main = all single digit numbers of base 10 starting from 0, followed by a new line.
+ * main = base 10 starting from 0, followed by a new line.
  * Return: 0
  */
 
