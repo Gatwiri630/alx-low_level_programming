17ece7276b2997e8f7fe485e9fbc6153788cc719
Gatwiri630 <gatwiriesther630@gmail.com>
2022-12-12 10:02:41 -0800

    fix

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - different combinations of two different numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
