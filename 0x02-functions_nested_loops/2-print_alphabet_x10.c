540514e37161ab0ce57923e0c78ef4907070d58d
Gatwiri630 <gatwiriesther630@gmail.com>
2022-12-15 08:15:45 -0800

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,14 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	char c, i;
 
-	while (i <=90)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
-		i++;
 	}
-
 }
-
