e2b2e11a641abafe2a79b17b94a5f2df8eab452b
Gatwiri630 <gatwiriesther630@gmail.com>
2022-12-15 08:56:06 -0800

    fix

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _slower -  check for lower character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _slower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
